a09615f428e6046f9dbaf0ee3d528fdc6a238f75
Angus McLeod <angus@mcleod.org.au>
2020-01-21 20:29:27 +1100

    tweaks

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -1,9 +1,15 @@
 class ReflectionsController < ::ApplicationController
   def index
-    render_serialized(User.find_by(username: params[:username]).reflections, ReflectionSerializer)
+    user = User.where(username: params[:username])
+    reflections = (user.present? && user.reflections.any?) ? user.reflections : []
+    render_serialized(reflections, ReflectionSerializer)
   end
   
   def create
+    
+  end
+  
+  def update
     
   end
   
--- lib/reflexivity/engine.rb
+++ lib/reflexivity/engine.rb
@@ -15,8 +15,9 @@ module ::Reflexivity
       # User.register_custom_field_type('enable_reflexivity', :boolean)
 
       # see lib/plugin/instance.rb for the methods available in this context
-      class ::User
-        has_many :reflections
+      
+      ::User.class_eval do
+        has_many :reflections, dependent: :destroy
       end
     end
   end
